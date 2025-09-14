ecaffbb7dd086e02a7b654dfc0a1ddc2999837bc
Kisawa16 <kukuhagung12@gmail.com>
2025-09-14 23:09:25 +0700

    (add) icon admin dashboard

--- admin/view/container.php
+++ admin/view/container.php
@@ -34,7 +34,10 @@ $current_page = $page;
         class="bg-white w-64 fixed inset-y-0 left-0 z-50 transform -translate-x-full md:translate-x-0 md:relative md:block shadow-lg transition-transform duration-300 ease-in-out">
         <div class="p-4 space-y-4 flex flex-col justify-between h-full">
           <?php include __DIR__ . '/partials/sidebar.php'; ?>
-          <a href="<?php echo base_url('admin/view/logout.php') ?>" class="block px-3 py-2 hover:bg-teal-100 text-gray-700 border-t-4 border-teal-600 <?php echo ($current_page == 'logout') ? 'bg-teal-100 font-medium' : ''; ?>">Logout</a>
+          <div class="flex pl-4 items-center hover:bg-teal-100 text-gray-700 border-t-4 border-teal-600 <?php echo ($current_page == 'logout') ? 'bg-teal-100 font-medium' : ''; ?>">
+              <i class="fa-solid fa-right-from-bracket"></i>
+              <a href="<?php echo base_url('admin/view/logout.php') ?>" class="block px-3 py-2 rounded-lg">Logout</a>
+          </div>
         </div>
       </aside>
 
--- admin/view/partials/sidebar.php
+++ admin/view/partials/sidebar.php
@@ -1,5 +1,14 @@
 <nav class="space-y-2">
-    <a href="container.php?page=dashboard" class="block px-3 py-2 rounded-lg hover:bg-teal-100 text-gray-700 <?php echo ($current_page == 'dashboard') ? 'bg-teal-100 font-medium' : ''; ?>">Dashboard</a>
-    <a href="container.php?page=laporan" class="block px-3 py-2 rounded-lg hover:bg-teal-100 text-gray-700 <?php echo ($current_page == 'laporan') ? 'bg-teal-100 font-medium' : ''; ?>">Laporan</a>
-    <a href="container.php?page=diagnosa" class="block px-3 py-2 rounded-lg hover:bg-teal-100 text-gray-700 <?php echo ($current_page == 'diagnosa') ? 'bg-teal-100 font-medium' : ''; ?>">Diagnosa</a>
+    <div class="flex pl-4 items-center hover:bg-teal-100 text-gray-700 <?php echo ($current_page == 'dashboard') ? 'bg-teal-100 font-medium' : ''; ?>">
+        <i class="fa-solid fa-house"></i>
+        <a href="container.php?page=dashboard" class="block px-3 py-2 rounded-lg">Dashboard</a>
+    </div>
+    <div class="flex pl-4 items-center hover:bg-teal-100 text-gray-700 <?php echo ($current_page == 'laporan') ? 'bg-teal-100 font-medium' : ''; ?>">
+        <i class="fa-solid fa-file-alt"></i>
+        <a href="container.php?page=laporan" class="block px-3 py-2 rounded-lg">Laporan</a>
+    </div>
+    <div class="flex pl-4 items-center hover:bg-teal-100 text-gray-700 <?php echo ($current_page == 'diagnosa') ? 'bg-teal-100 font-medium' : ''; ?>">
+        <i class="fa-solid fa-stethoscope"></i>
+        <a href="container.php?page=diagnosa" class="block px-3 py-2 rounded-lg">Diagnosa</a>
+    </div>
 </nav>
